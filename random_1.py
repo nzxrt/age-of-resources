cd7f21651425e16acd2e1f3986f5e7d2275924b7
ELC-901 <kvadiksofficial@gmail.com>
2026-02-28 20:43:59 +0300

    Delete random_1.py

--- random_1.py
+++
@@ -1,9 +0,0 @@
-from random import randint
-
-def sell_or_not(offered_price, quantity):
-    if randint(1, 100) <= 60:
-        print("✅ Продавец согласился!")
-        return True
-    else:
-        print("❌ Продавец отказался.")
-        return False
