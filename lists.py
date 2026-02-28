d5dd6769f2a965ca8d57914457d9833d8aed5a77
ELC-901 <kvadiksofficial@gmail.com>
2026-02-28 20:43:38 +0300

    Delete lists.py

--- lists.py
+++
@@ -1,6 +0,0 @@
-available_resources_for_sell = ['Coal', 'Iron']
-available_resources_for_buy = ["Coal", "Iron"]
-Coal_coast = 50
-Iron_coast = 10
-percent_for_lost_after_sell = 0.23
-country = ["СССР","США","КИТАЙ","ГЕРМАНИЯ","ПОЛЬША","ФИНЛЯНДИЯ",]
